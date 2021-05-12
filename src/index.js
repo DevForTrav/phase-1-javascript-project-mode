9610f007e6f365ad0d4659540b8ebb06b0681f60
Travis Courtney <travis@angry-fly.com>
2021-05-12 18:23:51 -0400

    Added Collection Feature

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const searchURL = "https://collectionapi.metmuseum.org/public/collection/v1/sear
 const main = document.getElementById('main')
 const showRandomPiece = document.getElementById('show-random-piece')
 const showSearchDiv = document.getElementById('show-search-div')
+const artCollectionLink = document.getElementById('art-collection-link')
 let featuredPiece;
 let randomPiece;
 let searchForm;
@@ -13,31 +14,45 @@ let loader;
 let pieceTitle;
 let pieceArtist;
 let pieceImage;
+let specificID;
 let ramdomizedID;
 let varName;
+let collectedIDs = [];
+let uniq;
+let artCollection;
+let likedImages = []
 
 
-const createImageCard = (id) => {
-    let doesDivExist = document.getElementById(id)
+const createImageCard = (id, htmlLocation) => {
+    console.log(`htmlLocation ${htmlLocation}`)
+    let doesDivExist = document.getElementById(htmlLocation)
+    console.log(doesDivExist)
     if (doesDivExist === null || doesDivExist === undefined) {
+        let divTitle = document.createElement('h2')
         parentDiv = document.createElement("div"),
         pieceTitle = document.createElement('h3'),
         pieceArtist = document.createElement('p'),
         pieceImage = document.createElement('img') 
+        specificID = document.createElement('p')
         loader = document.createElement('div')
+        divTitle.innerHTML = idToHeader(htmlLocation)
+        specificID.innerHTML = id
+        parentDiv.appendChild(divTitle)
         parentDiv.className = 'image-card'
-        parentDiv.id = id;
-        let htmlElements = [ pieceTitle, pieceArtist, pieceImage, loader ]
-        let classList = [ 'piece-title', 'artist-name', 'centered-image', 'loader']
+        parentDiv.id = htmlLocation;
+        let htmlElements = [ pieceTitle, pieceArtist, pieceImage, specificID, loader ]
+        let classList = [ 'piece-title', 'artist-name', 'centered-image','object-id', 'loader']
         htmlElements.forEach((element, i=0) => {
             element.className = classList[i]
             i++
             parentDiv.appendChild(element)
         });
+        doesDivExist = document.getElementById(htmlLocation)
         main.appendChild(parentDiv)
     } else {
-       doesDivExist.remove()
-       createImageCard(id)
+        doesDivExist.remove()
+        // console.log('This is passing undefined to the recursive function')
+        createImageCard(id, htmlLocation)
     }
     // varName = convertIDToVar(id)
     // // console.log(this.valueOf())s
@@ -92,6 +107,33 @@ const createImageCard = (id) => {
  
 }
 
+const createArtCollection = () => {
+    artCollection = document.createElement("div")
+    artCollection.id = "art-collection"
+    // artCollection.className = "image-card"
+    main.appendChild(artCollection)
+    let artUL = document.createElement("ul")
+    artCollection.appendChild(artUL)
+    likedImages.forEach((element, i = 1) => {
+        fetch(`${baseURL}/${element}`)
+        .then(response => response.json())
+        .then(data => {
+            let artLI = document.createElement('li')
+            let titleLI = document.createElement('p')
+            let imageLI = document.createElement('img')
+            imageLI.className = "collection-image"
+            artLI.className = "collection-li"
+            titleLI.innerHTML = data.title
+            imageLI.setAttribute('src', data.primaryImage )
+            artLI.appendChild(titleLI)
+            artLI.appendChild(imageLI)
+            artUL.appendChild(artLI)
+
+        })
+    });
+
+}
+
 const convertIDToVar = (string) => {
     let convertedString = string.split('-')
     convertedString[1] = convertedString[1].charAt(0).toUpperCase() + convertedString[1].slice(1)
@@ -100,12 +142,21 @@ const convertIDToVar = (string) => {
     
 }
 
-const createVar = (name, id) => {
-   return name = document.getElementById(id)
+const idToHeader = (string) => {
+    let convertedString = string.split('-')
+    convertedString[0] = convertedString[0].charAt(0).toUpperCase() + convertedString[0].slice(1)
+    convertedString[1] = convertedString[1].charAt(0).toUpperCase() + convertedString[1].slice(1)
+    convertedString = convertedString.join(' ')
+    return convertedString
+
 }
 
+// const createVar = (name, id) => {
+//    return name = document.getElementById(id)
+// }
+
 const getArtImage = (id, htmlLocation) => {
-    createImageCard(htmlLocation)
+    createImageCard(id, htmlLocation) 
     fetch(`${baseURL}/${id}`)
     .then(response => response.json())
     .then((data) => {
@@ -117,19 +168,20 @@ const getArtImage = (id, htmlLocation) => {
     })
 }
 
+
 const postImageToDOM = (artPiece, htmlLocation) => {
     let location = document.getElementById(htmlLocation)
     let img = parentDiv.getElementsByClassName('centered-image');
     let artImage = artPiece.primaryImage;
     img[0].setAttribute('src', artImage)
     parentDiv.querySelector(".piece-title").innerHTML = artPiece.title;
-    postArtist(artPiece, location); 
+    postArtist(artPiece, location);
     location.appendChild(img[0])
     loader.remove()
+    addPieceInteraction(htmlLocation)
     
 }
 
-const hideLoader = () => { loader.style.display = "none"}
 
 const postArtist = (artPiece, domLocation) => {
     if (artPiece.artistDisplayName) {
@@ -192,16 +244,35 @@ const addNextButtons = () => {
 
 }
 
+const likeImage = (id) => {
+    likedImages.push(parseInt(id))
+}
+// let array = [1, 1, 2, 2, 3, 3, 5]
+const uniqueArray = (array) => {
+    // return a new array of all unique values of the array
 
-const addPieceInteraction = () => {
 
+    let unique = []
+    array.forEach((element) => {
+        if (!unique.includes(element)) {
+            unique.push(element)
+        }
+    })
+    return unique;
 }
 
-const stageRandomDiv = () => {
-   
+
+const addPieceInteraction = (htmlLocation) => { 
+    const btn = ["like", "comment"]
+    btn.forEach(element => {
+        button = document.createElement('button')
+        button.id = `${htmlLocation}-btn`
+        button.className = `${element}-btn`
+        button.innerHTML = element
+        parentDiv.appendChild(button)
+        
+    });  
 }
-
-
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -209,7 +280,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-showRandomPiece.addEventListener("click", (function(e) {
+showRandomPiece.addEventListener("click", (() => {
     postRandomArt()
 }))
 
@@ -224,8 +295,25 @@ showSearchDiv.addEventListener("click", (() => {
         searchPiece = document.getElementById('search-piece')
         return getSearchImage(searchValue)
             .then(data => {
-                console.log(searchPiece) 
                 return getArtImage(data , `search-piece`)
             })
     })
 }))
+
+main.addEventListener('click', (e) => {
+    if(e.target.className === 'like-btn'){
+        console.log(e.target.parentElement.children[3].innerHTML)
+        console.log(e.target)
+        likeImage(e.target.parentElement.children[3].innerHTML)
+    } else if (e.target.className === '') {
+        // creat
+    }
+})
+
+artCollectionLink.addEventListener("click", () => {
+    if (artCollection === undefined) {
+        createArtCollection()   
+    }
+
+})
+
